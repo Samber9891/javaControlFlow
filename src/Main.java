3c7d04a525040898833d1cc7d8f934f51e776702
Samber9891 <Slem0119@gmail.com>
2020-10-11 12:18:02 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+// Part 1 
+// the value of k is B.80 
+// this is because the i < 5 is false, thus stopping the code from continuing any futher and never actually reaching k = k-12
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] newArr = {"*", "**", "***", "****", "*****"};
+        
+       
+            for(int i = 0; i< newArr.length; i++){
+
+                for (int j = 0; j < 1; j++) {
+                    
+                    System.out.println(newArr[i]);  
+                }
+        
+            }  
+        }
+         
+    }
+
+
